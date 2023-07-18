c5b3110a3e5c894d6940128161fe62808cd8e11a
Adil <adil.jawaid@outlook.com>
2023-07-18 12:28:22 +0100

    Creating functionality to view single article

--- pages/ViewArticle.jsx
+++ pages/ViewArticle.jsx
@@ -1,5 +1,48 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "../components/Navbar";
+
 function ViewArticle() {
-  return <h1>Hello I am article view page</h1>;
+  <Navbar></Navbar>;
+  const [article, viewArticle] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { article_id } = useParams();
+  useEffect(() => {
+    axios
+      .get(
+        `https://news-application-f2jb.onrender.com/api/articles/${article_id}`
+      )
+      .then((article) => {
+        viewArticle(article.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+      });
+  }, [article_id]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const publishedDate = article.created_at.substring(0, 10);
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      {
+        <div className="view-article-info">
+          <h1 className="view-article-title">{article.title}</h1>
+          <p>Author: {article.author}</p>
+          <p>Votes: {article.votes}</p>
+          <p>Published: {publishedDate}</p>
+          <p>Topic: {article.topic}</p>
+          <p>{article.body}</p>
+        </div>
+      }
+    </div>
+  );
 }
 
 export default ViewArticle;
