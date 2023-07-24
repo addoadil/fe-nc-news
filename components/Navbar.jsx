6f2be197f25f831f5566b774b0344cba13ec156d
Adil <adil.jawaid@outlook.com>
2023-07-24 20:50:52 +0100

    Improving styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,30 @@
 function Navbar() {
-    return (
-      <div className="container">
-        <nav>
-          <h2 className="logo-text">NC NEWS</h2>
-          <button className="search">🔎</button>
-          <button className="account">👤</button>
-        </nav>
-      </div>
-    );
-  }
-  
-  export default Navbar;
-  
+  return (
+    <div className="container">
+      <nav>
+        <h2 className="logo-text">NC NEWS</h2>
+        <h4 className="menu-item-1">Articles</h4>
+        <h4 className="menu-item-2">About Us</h4>
+        <h4 className="menu-item-3">Contact</h4>
+        <div className="icons-container">
+          <button className="search">
+            <img
+              src="icons/search-icon.png"
+              alt="search-icon"
+              className="search-icon"
+            ></img>
+          </button>
+          <button className="account">
+            <img
+              src="/icons/account-white-icon.png"
+              className="account-icon"
+              alt="account-icon"
+            ></img>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
